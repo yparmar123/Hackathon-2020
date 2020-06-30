d6bf002f173606ccc300c30c5e49efe9370b6c80
yparmar2080 <yuvrajparmar2080@gmail.com>
2020-06-30 11:44:06 -0400

    yejin's updates

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 
-require('dotenv').config({path:"./config/keys.env"});
-
 const app = express();
-
-app.use(express.static('public'));
-
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("home", {
-        title: "Home Page"
-    });
+
+  res.render("home", {
+    mainid: "main-box"
+  });
 });
 
-app.get("/about", (req, res) => {
-    res.render("about", {
-        title: "About Us Page"
-    });
+app.get("/signup", (req, res) => {
+  res.render("signup", {
+    mainid: "Sign up"
+  });
 });
 
-app.get("/contact", (req, res) => {
-    res.render("contact", {
-        title: "Contact Us Page"
-    });
+app.get("/login", (req, res) => {
+  res.render("login", {
+    mainid: "login"
+  });
 });
 
-app.listen(3000, ()=> {
-    console.log("Web Server is up and running!");
+app.listen(3000, () => {
+  console.log("Webserver connectedWeb server is up and running");
 });
