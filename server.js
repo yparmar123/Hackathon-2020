c39c425bf5ff6b519fc3757f489cf87212e524d1
yjkim9597 <59785342+yjkim9597@users.noreply.github.com>
2020-06-30 14:12:18 -0400

    new pages

--- server.js
+++ server.js
@@ -25,7 +25,42 @@ app.get("/login", (req, res) => {
     mainid: "login"
   });
 });
+app.get("/contactus", (req, res) => {
+  res.render("contactus", {
+    mainid: "main-box"
+  });
+});
+app.get("/estimates", (req, res) => {
+  res.render("estimates", {
+    mainid: "main-box"
+  });
+});
+app.get("/expenses", (req, res) => {
+  res.render("expenses", {
+    mainid: "main-box"
+  });
+});
+app.get("/feedback", (req, res) => {
+  res.render("feedback", {
+    mainid: "main-box"
+  });
+});
+app.get("/invoices", (req, res) => {
+  res.render("invoices", {
+    mainid: "main-box"
+  });
+});
+app.get("/payment", (req, res) => {
+  res.render("payment", {
+    mainid: "main-box"
+  });
+});
 
+app.get("/setting", (req, res) => {
+  res.render("setting", {
+    mainid: "main-box"
+  });
+});
 app.listen(3000, () => {
   console.log("Webserver connectedWeb server is up and running");
 });
